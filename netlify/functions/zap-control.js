a554351c2e2732c679e9f3dde3258ecfc398db0b
Evaeadao2005 <86280981+Evaeadao2005@users.noreply.github.com>
2026-02-18 19:30:11 -0300

    Update zap-control.js

--- netlify/functions/zap-control.js
+++ netlify/functions/zap-control.js
@@ -11,7 +11,7 @@ const ZAP_CONTROL = {
     ZAP05: { number: '558894959133', active: true, rotate: true },
     ZAP06: { number: '558894963227', active: true, rotate: true },
     ZAP07: { number: '558894968232', active: true, rotate: true },
-    ZAP08: { number: '558894976237', active: false, rotate: false },
+    ZAP08: { number: '558894976237', active: true, rotate: false },
     ZAP09: { number: '558894927965', active: true, rotate: true }
   }
 };
